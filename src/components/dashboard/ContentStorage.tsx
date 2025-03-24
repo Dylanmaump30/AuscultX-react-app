ea7262a1ee2a6af5e744f52954f5fd27253c9027
Dylanmaump30 <dylanmauricio05@gmail.com>
2025-03-24 10:44:48 -0500

    fix some issues

--- src/components/dashboard/ContentStorage.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext, useState, useEffect } from "react";
-import { MyContext } from "../../App";
-import { AudiosInfo } from "../../models/user.model";
-
-function ContentStorage(_id: AudiosInfo) {
-  return <div className="content-storage-container"></div>;
-}
-export default ContentStorage;
